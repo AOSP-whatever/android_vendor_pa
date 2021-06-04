b9506694b8bef49166feab96d28257cf240a13a4
RahifM <faizel326@gmail.com>
2021-06-04 13:19:15 +0530

    mido: Add time of days flag
    Signed-off-by: RahifM <faizel326@gmail.com>
    Change-Id: I0e5cbe8116fe9f9bb21de5b7275f5903df3f836a

--- products/mido/pa_mido.mk
+++ products/mido/pa_mido.mk
@@ -28,6 +28,7 @@ TARGET_BOOT_ANIMATION_RES := 1080
 include vendor/pa/config/common_full_phone.mk
 
 TARGET_DISABLES_GMS := true
+PA_VERSION_APPEND_TIME_OF_DAY := true
 
 # Inherit from mido device
 $(call inherit-product, device/xiaomi/mido/device.mk)
